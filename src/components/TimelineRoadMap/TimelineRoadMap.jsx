71ce4096ce30bd1c51bb32b5f4b9e5fdd4c55979
wwdfdo <wwdfdo@gmail.com>
2022-06-06 19:02:55 +0530

    roadmap title

--- src/components/TimelineRoadMap/TimelineRoadMap.jsx
+++ src/components/TimelineRoadMap/TimelineRoadMap.jsx
@@ -3,8 +3,8 @@ import roadmapImg from "../../images/roadmapImg.png";
 
 const TimelineRoadMap = () => {
   return (
-    <div id="Roadmap" className="pt-24 bg-[#283259] drop-shadow-2xl">
-      <h2 className="text-white  font-rubik font-bold lg:text-[2.5em] text-6xl text-center">
+    <div id="Roadmap" className="lg:pt-24 pt-12 bg-[#283259] drop-shadow-2xl">
+      <h2 className="text-white  font-rubik font-bold lg:text-[2.5em] text-3npxl text-center">
         Road Map
       </h2>
       <div className="bg-roadmapbg bg-cover bg-no-repeat mx-auto lg:pt-64 pt-20">
