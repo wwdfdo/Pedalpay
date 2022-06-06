ce9122203efea1699e1b4d68487929f4d7328764
wwdfdo <wwdfdo@gmail.com>
2022-06-06 17:39:17 +0530

    change roadmap image

--- src/components/TimelineRoadMap/TimelineRoadMap.jsx
+++ src/components/TimelineRoadMap/TimelineRoadMap.jsx
@@ -1,14 +1,14 @@
 import React from "react";
-import timelinebg from "../../images/roadmapsv.svg";
+import roadmapImg from "../../images/roadmapImg.png";
 
 const TimelineRoadMap = () => {
   return (
     <div id="Roadmap" className="pt-24 bg-[#283259] drop-shadow-2xl">
-      <h2 className="text-white font-rubik font-bold lg:text-[2.5em] text-6xl text-center">
+      <h2 className="text-white  font-rubik font-bold lg:text-[2.5em] text-6xl text-center">
         Road Map
       </h2>
       <div className="bg-roadmapbg bg-cover bg-no-repeat mx-auto pt-64">
-        <img className="w-full mx-auto" src={timelinebg} alt="" />
+        <img className="w-full mx-auto" src={roadmapImg} alt="" />
       </div>
     </div>
   );
