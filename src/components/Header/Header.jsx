e22758d69c7c7826a47e8b60ff360c8cd3843843
wwdfdo <wwdfdo@gmail.com>
2022-06-07 00:28:42 +0530

    mobile hamburger

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,32 +2,18 @@ import React from "react";
 import Logo from "./Logo";
 import { useState } from "react";
 import { menus } from "../../arrays/menuArray";
+import Navigation from "./Navigation";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   const [navId, setNavId] = useState();
 
   return (
-    <div className="w-full h-[74px] fixed z-20 bg-[#344174] font-[Fira Sans]">
-      <div className="w-4/5 flex justify-between items-center mx-auto pt-[15px]">
+    <div className="w-full lg:h-[85px] h-[60px] fixed z-20 bg-[#14213d] font-[Fira Sans]">
+      <div className="lg:w-4/5 lg:flex justify-between items-center mx-auto pt-[15px]">
         <Logo width={"w-[200px]"} />
-        <div className="flex gap-8 items-center text-sm text-[#E0EAEC] font-semibold ">
-          {menus.map((menu) => (
-            <a
-              key={menu.id}
-              className={`${
-                navId === menu.id ? "text-[#73f59b]" : "text-white"
-              } ${
-                menu.id === "button"
-                  ? " border-2 border-[#2E7339] py-2 px-5 rounded-md text-white"
-                  : ""
-              } lg:block hidden`}
-              onClick={() => setNavId(menu.id)}
-              href={menu.link}
-            >
-              {menu.title}
-            </a>
-          ))}
-        </div>
+        <Navigation menus={menus} navId={navId} setNavId={setNavId} />
+        <MobileNav menus={menus} />
       </div>
     </div>
   );
