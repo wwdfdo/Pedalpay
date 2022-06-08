236ee2a3882a0c02569c4e0b7574d441e9999cd2
wwdfdo <wwdfdo@gmail.com>
2022-06-08 12:13:29 +0530

    viewport changed

--- src/components/ViewPort/ViewPort.jsx
+++ src/components/ViewPort/ViewPort.jsx
@@ -7,36 +7,23 @@ import video2 from "../../images/viewportVideo3.mkv";
 
 const ViewPort = () => {
   return (
-    <div className="flex flex-col md:flex-row md:w-4/5 mx-auto gap-12 justify-between items-center pt-24">
-      <div className="mt-8 w-5/6 md:w-2/5 text-white  flex flex-col gap-12 ">
+    <div className="flex flex-col md:flex-col md:w-4/5 mx-auto gap-12 justify-between items-center pt-24">
+      <div className="mt-8 text-white  flex flex-col gap-4 ">
         <div>
-          <h1 className=" text-[3.5rem] font-bold font-opensans text-titlecolor leading-tight">
+          <h1 className=" text-[3.5rem] font-bold text-center font-opensans text-titlecolor leading-tight">
             Bikes Are Not a Sign of Poverty, They Are a Great{" "}
             <span className="text-[#73f59b]"> Equalizer</span>
           </h1>
         </div>
         <div>
-          <p className=" text-[1.3rem] leading-relaxed font-semibold text-[#CAD1CE]">
+          <p className=" text-[1.3rem] w-2/3 mx-auto leading-relaxed text-center font-semibold text-[#CAD1CE]">
             PedalPay is a futuristic, eco-mobility platform combining worlds of
             Game-Fi and Social-Fi into the most green mode of mobility, cycling.
           </p>
         </div>
-        <div className="flex gap-6">
-          <button>
-            <img className=" w-[160px]" src={Applestore} alt="" />
-          </button>
-          <button>
-            <img className=" w-[160px]" src={Googleplay} alt="" />
-          </button>
-        </div>
       </div>
 
-      <video
-        className="lg:absolute lg:right-0 lg:w-3/5"
-        loop="true"
-        autoplay="autoplay"
-        muted
-      >
+      <video className="lg:w-4/5" loop="true" autoplay="autoplay" muted>
         <source src={video2} type="video/mp4"></source>
       </video>
       {/* <img
@@ -44,6 +31,15 @@ const ViewPort = () => {
         src={viewportImg}
         alt=""
       /> */}
+
+      <div className="flex gap-6">
+        <button>
+          <img className=" w-[160px]" src={Applestore} alt="" />
+        </button>
+        <button>
+          <img className=" w-[160px]" src={Googleplay} alt="" />
+        </button>
+      </div>
     </div>
   );
 };
