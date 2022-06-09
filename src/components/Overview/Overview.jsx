8d6f9a62f00920f0d6ef8fd797ed163371ba1d2b
wwdfdo <wwdfdo@gmail.com>
2022-06-09 12:31:28 +0530

    change overview

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -11,14 +11,14 @@ const Overview = () => {
         </h2>
         <div className="flex lg:flex-row flex-col w-4/5 justify-between mx-auto items-center gap-20">
           <img className="lg:w-1/2" src={overviewImg} alt="" />
-          <div className="neon-content lg:w-1/2 text-[#fff] py-10 px-10 rounded-lg border-[4px] border-[#ffffff] shadow-[0px 4px 30px 3px #ffffff]   relative h-[100%] text-center">
+          <div className="lg:w-1/2 text-titlecolor  py-10 px-10 rounded-lg font-mono">
             <div>
-              <h3 className="neon-header text-2xl font-bold mb-6 font-rubik">
+              <h3 className=" neon text-2xl font-bold mb-6 font-rubik">
                 EXPERIENCE JUST FOR RIDER OFFER
               </h3>
             </div>
             <div>
-              <p className=" leading-loose text-semibold text-lg ">
+              <p className="neon leading-loose text-semibold text-lg ">
                 Pedalpay is a tokenized sports application based on web3. Users
                 can purchase NFTs in the form of virtual bicycles which are
                 linked to real riding activities. By riding around, users will
