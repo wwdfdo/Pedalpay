b88db176349e7ddc463118f75421a2bb1f608c1f
wwdfdo <wwdfdo@gmail.com>
2022-06-10 14:11:34 +0530

    neon roadmap positioning

--- src/components/NeonRoadMap/NeonRoadMap.jsx
+++ src/components/NeonRoadMap/NeonRoadMap.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-// import NeonRoadMapImg from "../../images/neonroadmap3.png";
+import NeonRoadMapImg from "../../images/neonroadmap3.png";
 
 const NeonRoadMap = () => {
   return (
-    <div className="w-full mb-32 mt-36 bg-neonroadmap bg-no-repeat bg-cover h-[855px]">
-      <h2 className="text-textcolor font-rubik p-10 font-bold text-center text-[2.5em]">
+    <div className="w-full mb-32 mt-36 relative">
+      <h2 className="text-textcolor font-rubik font-bold text-center text-[2.5em] absolute left-[50%] -ml-10 mt-10">
         Road Map
       </h2>
-      {/* <img src={NeonRoadMapImg} alt="" /> */}
+      <img src={NeonRoadMapImg} alt="" />
     </div>
   );
 };
