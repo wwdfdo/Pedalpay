f2e57106b825df405afe035e23f8db251bbb0671
wwdfdo <wwdfdo@gmail.com>
2022-06-10 11:29:59 +0530

    nft economy

--- src/components/NftEconomy/NftEconomy.jsx
+++ src/components/NftEconomy/NftEconomy.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import economyImg from "../../images/Neon10.png";
+import economyImg from "../../images/nfteconomy.png";
 
 const NftEconomy = () => {
   return (
-    <div className="flex flex-col gap-10 justify-center mx-auto w-2/5 mt-40 mb-36">
-      <h2 className="text-textcolor font-rubik font-bold text-center text-[2.5em] mb-24 mt-10">
+    <div className="flex flex-col gap-10 justify-center mx-auto w-full mt-40 mb-36">
+      <h2 className="text-textcolor font-rubik font-bold text-center text-[2.5em] mb-16 mt-10">
         NFT Echonomy
       </h2>
       <img src={economyImg} alt="" />
